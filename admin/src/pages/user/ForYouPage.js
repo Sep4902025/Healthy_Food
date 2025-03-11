900aa10c50d762ff1ed8f8508f2386e1e6c8f0d5
Le Thanh Sang <sangltce140611@fpt.edu.vn>
2025-03-11 22:10:25 +0700

    push 11/3

--- admin/src/pages/user/ForYouPage.js
+++ admin/src/pages/user/ForYouPage.js
@@ -2,57 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectAuth } from "../../store/selectors/authSelectors";
-// Import the icons
-import homePicture from "../../assets/images/homePic.png";
+
 import brow from "../../assets/images/brow.png";
 import dessert from "../../assets/images/dessert.png";
 import dish from "../../assets/images/mainDish.png";
 import breakfast from "../../assets/images/breakfast.png";
-import HomeService from "../../services/home.service";
-import FoodSlider from "../../components/ui/FoodSlider";
+const ForYoyPage = () => {
+    const navigate = useNavigate();
 
-const Home = () => {
-  const navigate = useNavigate();
-  const [dishes, setDishes] = useState([]);
-  const userId = useSelector(selectAuth)?.user?._id;
-
-  useEffect(() => {
-    const fetchDishes = async () => {
-      try {
-        const data = await HomeService.getAllDishes();
-        setDishes(data.data);
-        console.log("Full dishes:", dishes);
-      } catch (error) {
-        console.error("Error fetching food data:", error);
-      }
-    };
-
-    fetchDishes();
-  }, []);
   return (
     <div className="home">
-      <div className="home-container">
-        <div className="home-content">
-          <h1 className="home-title bold text-black-600">
-            <span className="text-green">Do you want to personalize it for you?</span>
-          </h1>
-          <p className="home-description">
-          Please tell us more about yourself.
-          </p>
-          <button
-            onClick={() => navigate("/another-page")} // Replace "/another-page" with the desired path
-            className="btn-discover bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200"
-          >
-            Take survey here
-          </button>
-        </div>
-        <div className="home-picture">
-          <img src={homePicture} alt="home-picture" className="picture" />
-        </div>
-      </div>
+      
 
       <div className="cate-container">
-        <h1 className="cate-title">Popular Categories</h1>
+        <h1 className="cate-title">Recipes by category</h1>
         <div className="cate-list">
           <div className="cate-item">
             <div className="center-con">
@@ -94,14 +57,14 @@ const Home = () => {
       </div>
 
       <div className="food-container">
-        <h3 className="food-slogan">Recommended Dishes</h3>
+        <h3 className="food-slogan">Recommended For you</h3>
         <div className="food-title-container">
-          <h1 className="food-title">Standout Foods From Our Menu</h1>
+          
           
         </div>
         
 
-        <FoodSlider userId={userId} dishes={dishes} />
+        
 
         <div className="chef-container">
           <div className="chef-img-container"></div>
@@ -112,4 +75,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default ForYoyPage;
--- admin/src/routes/index.js
+++ admin/src/routes/index.js
@@ -56,6 +56,7 @@ import WaterDrink from "../pages/quizinfor/WaterDrink";
 import Weight from "../pages/quizinfor/Weight";
 import WeightGoal from "../pages/quizinfor/WeightGoal";
 import ViewQuiz from "../pages/user/ViewQuiz";
+import ForYoyPage from "../pages/user/ForYouPage";
 const AppRoutes = () => {
   return (
     <Routes>
@@ -95,7 +96,7 @@ const AppRoutes = () => {
             </PrivateRoute>
           }
         />
-        
+
         <Route
           path="meal"
           element={
@@ -115,6 +116,15 @@ const AppRoutes = () => {
 
         <Route path="ingredients/:type" element={<IngredientList />} />
         <Route path="dishes/:type" element={<DishesList />} />
+
+        <Route
+        path="/foryou"
+        element={
+          <PrivateRoute>
+            <ForYoyPage />
+          </PrivateRoute>
+        }
+      ></Route>
 
         {/* ✅ Bảo vệ trang chat, chỉ user mới vào được */}
         <Route
@@ -189,6 +199,8 @@ const AppRoutes = () => {
         }
       ></Route>
 
+      
+
       {/* ✅ Bảo vệ toàn bộ route nutritionist */}
       <Route
         path="/nutritionist"
@@ -211,8 +223,6 @@ const AppRoutes = () => {
           <Route path="add" element={<AddIngredient />} />
         </Route>
       </Route>
-
-      
     </Routes>
   );
 };
